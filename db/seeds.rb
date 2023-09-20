952b1549e64404d5197235aa0d9cc0342f617eb4
paul-tes <pauasca8462@gmail.com>
2023-09-20 18:16:28 +0300

    Remove double qutes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,14 +9,14 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 greetings = [
-  "Sample Greeting 1",
-  "Sample Greeting 2",
-  "Sample Greeting 3",
-  "Sample Greeting 4",
-  "Sample Greeting 5",
-  "Sample Greeting 6",
-  "Sample Greeting 7",
-  "Sample Greeting 8"
+  'Sample Greeting 1',
+  'Sample Greeting 2',
+  'Sample Greeting 3',
+  'Sample Greeting 4',
+  'Sample Greeting 5',
+  'Sample Greeting 6',
+  'Sample Greeting 7',
+  'Sample Greeting 8'
 ]
 
 greetings.each do |greet|
