6ed94c41feb88a9a30317a841a734374e8533a9a
paul-tes <pauasca8462@gmail.com>
2023-09-20 18:15:54 +0300

    Add sample datas to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greetings = [
+  "Sample Greeting 1",
+  "Sample Greeting 2",
+  "Sample Greeting 3",
+  "Sample Greeting 4",
+  "Sample Greeting 5",
+  "Sample Greeting 6",
+  "Sample Greeting 7",
+  "Sample Greeting 8"
+]
+
+greetings.each do |greet|
+  Greeting.new(greeting: greet)
+end
